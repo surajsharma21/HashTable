419514286b16f44fd8c0ea60df5f34e9428eb1fd
Suraj <surajshrm000@gmail.com>
2020-10-28 22:30:11 +0530

    [Suraj] Add: Implementing Hash Table

---
+++ HashTable/Operation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class Operation
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void Frequency(MyMapNode<int, string> hash)
+        {
+            for (int key = 0; key < hash.size; key++)
+            {
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine($"frequency of word '{item.Key}' is {item.Value}");
+            }
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
+            hash.Add(0, "To");
+            hash.Add(1, "be");
+            hash.Add(2, "or");
+            hash.Add(3, "not");
+            hash.Add(4, "to");
+            hash.Add(5, "be");
+            Operation operation = new Operation();
+            operation.Frequency(hash);
+        }
+    }
+}
