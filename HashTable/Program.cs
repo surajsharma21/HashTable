c7534a0adff0e6181c025635713925ba17d61a92
Suraj <surajshrm000@gmail.com>
2020-10-28 22:34:59 +0530

    [Suraj] Add: Frequency of words in Paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -6,13 +6,15 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
-            MyMapNode<int, string> hash = new MyMapNode<int, string>(6);
-            hash.Add(0, "To");
-            hash.Add(1, "be");
-            hash.Add(2, "or");
-            hash.Add(3, "not");
-            hash.Add(4, "to");
-            hash.Add(5, "be");
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] para = paragraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            int key = 0;
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
             Operation operation = new Operation();
             operation.Frequency(hash);
         }
